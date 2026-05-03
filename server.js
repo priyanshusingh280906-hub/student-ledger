dd53ab5a519230169f74cb254c0ee04fc41b1fd3
Priyanshu Singh <priyanshusingh280906@gmail.com>
2026-05-03 20:25:29 +0530

    added a fake database and get function or display student

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const app = express();
 
 app.use(express.json()); 
 
+let students =[];
+
 app.post("/students", (req, res) => {
   const data = req.body;
 
   console.log(data);
 
-  res.send("Student received!");
+  res.send("Student added!");
+});
+
+app.get("/students", (req, res) => {
+  res.json(students);
 });
 
 app.listen(3000, () => {
